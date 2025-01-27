3b1a36cb20af3a8ed7eb0d2bd5292325ec6e92a0
Nikhil79922 <ns94301918@gmail.com>
2025-01-27 12:06:52 +0530

    minor fix

--- Services/auth.js
+++ Services/auth.js
@@ -8,7 +8,7 @@ function setUser(user) {
       _id:user._id,
       email:user.email,
       role:user.role
-    },Secret,{expiresIn:"10s"})
+    },Secret,{expiresIn:"1h"})
 
 }
 function getUser(token) {
